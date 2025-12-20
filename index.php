082f4d073e994537fe47454339edaa59d44426c5
Ken Keller <ken.keller@tptlive.ee>
2025-12-20 15:40:17 +0200

    Add magic methods examples

--- index.php
+++ index.php
@@ -5,10 +5,37 @@ class Box {
     protected $height; //väärtust saab lugeda klasssi sees, väljapool ei saa, subclassis saab ka lugeda
     private $length; //ainult klassi sees kus see sätitud on
 
+    public function __construct($width, $height, $length) {
+        $this->height = $height;
+        $this->length = $length;
+        $this->width = $width;
+
+        var_dump('Object created');
+    }
+    
+    public function __destruct()
+    {
+        var_dump('Object destroyed');
+    }
+
+    public function __toString()
+    {
+        return 'some box';
+    }
+
+    public function __get($name){
+        var_dump($name);
+        return 'some value';
+    }
+
+    public function __set($name, $value){
+        var_dump($name, $value);
+    }
+
     public function setLength(int $length) {
         if($length<0){
             throw new Exception('You are stupid');
-       }
+        }
         $this->length = $length;
     }
 
@@ -25,8 +52,15 @@ class MetalBox extends Box {
     }
 }
 
-$metal1 = new MetalBox();
-$metal1->width = 10;
-//$metal1->height = 10;
-$metal1->setLength(-123);
+function makeBox() {
+    $metal1 = new MetalBox(10, 10, 10);
+    var_dump($metal1);
+}
+makeBox();
+$metal1 = new MetalBox(10, 10, 10);
 var_dump($metal1);
+echo $metal1;
+var_dump($metal1->coolbeans);
+$metal1->catlitter = 'asdasdasd';
+
+var_dump('end');
