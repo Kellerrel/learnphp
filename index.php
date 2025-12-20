be1e22913bfc18641b5102443be0ff3fa88f9600
Ken Keller <ken.keller@tptlive.ee>
2025-12-20 16:17:07 +0200

    Add static example

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <?php
 
 class Box {
-    public $num;
+    public static $count;
+    public $width;
+    public static function getCount() {
+        var_dump(self::class);
+        var_dump(self::$count);
+        var_dump(static::class);
+        var_dump(static::$count);
+        //var_dump($this->width);
+    }
+    public function getWidth(){
+         var_dump(self::class);
+        var_dump(self::$count);
+        var_dump(static::class);
+        var_dump(static::$count);
+        var_dump($this->width);
+    }
+}
+class MetalBox extends Box {
+
 }
 
-$num1 = 1;
-$num2 = &$num1;
-$num2 = 2;
-var_dump($num1, $num2);
 
+Box::$count = 1;
+Box::$count = 2;
+var_dump(Box::$count, Box::$count);
+Box::getCount();
+var_dump(Box::class);
+MetalBox::getCount();
 $box1 = new Box();
-$box1->num = 1;
-$box2 = clone $box1;
-$box2->num = 2;
-var_dump($box1->num, $box2->num);
-var_dump($box1, $box2);
-
-$array = [1,2,3,4,5];
-
-for($i=0;$i<count($array);$i++){
-    $num = &$array[$i];
-    $num *=2;
-}
-
-foreach($array as &$num) {
-    $num *=2;
-    var_dump($num);
-}
-var_dump($array);
+$box1->getWidth();
