605d4c92aa383e5951e1f6060f5cf03b3f716ba5
Ken Keller <ken.keller@tptlive.ee>
2025-12-20 15:20:02 +0200

    Add visibility example

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 
 class Box {
-    public $width;
-    public $height;
-    public $length;
+    public $width; //väärtust saab kirjutada ja lugeda kõikjal
+    protected $height; //väärtust saab lugeda klasssi sees, väljapool ei saa, subclassis saab ka lugeda
+    private $length; //ainult klassi sees kus see sätitud on
+
+    public function setLength(int $length) {
+        if($length<0){
+            throw new Exception('You are stupid');
+       }
+        $this->length = $length;
+    }
 
     public function volume() {
         return $this->width * $this->height * $this->length;
     }
 }
 
-class MetalBox extends Box { //extends võtab boxi aluseks
+class MetalBox extends Box {
     public $weightPerUnit;
 
     public function weight() {
@@ -20,10 +27,6 @@ class MetalBox extends Box { //extends võtab boxi aluseks
 
 $metal1 = new MetalBox();
 $metal1->width = 10;
-$metal1->height = 10;
-$metal1->length = 10;
-$metal1->weightPerUnit = 2;
+//$metal1->height = 10;
+$metal1->setLength(-123);
 var_dump($metal1);
-var_dump($metal1->volume());
-var_dump($metal1->weight());
-
