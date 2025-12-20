d4b2c5f558b7480a12ed845dbd80563beae19c26
Ken Keller <ken.keller@tptlive.ee>
2025-12-20 11:43:34 +0200

    Add weird floating point

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 
-$text = 'hello';
-$text = $text . ' ken';
-$text .= '!!';
-$text = ucwords($text);
-var_dump($text);
-var_dump($text[2]); //sõne - string
+$num = 0.1 + 0.2;
+var_dump($num);
