a3ae2b91e996d5d210b68fd26ca98300c736b53c
Ken Keller <ken.keller@tptlive.ee>
2025-12-20 11:24:39 +0200

    Add type example

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo 'Hello World';
-?>
+
+$num = 10;
+$num = 10.2;
+$name = 'Ken';
+$num = 'Hello\n$name';
+$num = "Hello\n$name";
+$num = <<<END
+Hello
+$name
+END;
+$num = true;
+$num = false;
+$num = null;
+$num = array(1, 2, 3, 4);
+$num = [1, 2, 3, 4];
+$num = new stdClass();
+var_dump($num);
