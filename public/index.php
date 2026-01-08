cd6b50a200ef3cc5bbd0669f99e594f35de95024
Ken Keller <ken.keller@tptlive.ee>
2026-01-08 10:45:14 +0200

    Add autoload and namespace

--- public/index.php
+++ public/index.php
@@ -2,23 +2,41 @@
 function dump(...$vars)
 {
     echo '<pre>';
-    var_dump($_SERVER);
+    var_dump(...$vars);
     echo '</pre>';
 }
 
-switch($_SERVER['REQUEST_URI']){
-    case '/':
-        $title = 'World News';
-        include '../views/page.php';
-        break;
-     case '/us':
-        $title = 'U.S News';
-        include '../views/page.php';
-        break;
-    case '/tech':
-        $title = 'Tech News';
-        include '../views/page.php';
-        break;
-    default:
-        echo '404- page not found';
-    };
+spl_autoload_register(function ($class) {
+    dump($class);
+    $class = str_replace('App\\', '', $class);
+    require_once "../src/$class.php";
+});
+
+use App\Controllers\PublicController;
+use App\Router;
+
+$router = new Router();
+dump($router);
+
+$db = new App\DB();
+dump($db);
+
+$controller = new PublicController();
+dump($controller);
+
+// switch($_SERVER['REQUEST_URI']){
+//     case '/':
+//         $title = 'World News';
+//         include '../views/page.php';
+//         break;
+//      case '/us':
+//         $title = 'U.S News';
+//         include '../views/page.php';
+//         break;
+//     case '/tech':
+//         $title = 'Tech News';
+//         include '../views/page.php';
+//         break;
+//     default:
+//         echo '404- page not found';
+//     };
